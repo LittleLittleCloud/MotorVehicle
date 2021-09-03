21b6f8b36095c409be6a9d2bd5d3384c52605f9a
XiaoYun Zhang <xiaoyuz@microsoft.com>
2021-09-03 14:25:39 -0700

    init

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SampleClassiciation;
+
+var output = SampleClassification.Predict(horizon: 30);
+Console.WriteLine(string.Join("\r\n", output.x));
